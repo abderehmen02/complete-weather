174e4595b550ad596cd667ccdcd8665d85b8a82e
abderehmen02 <ilahalaha0987@gmail.com>
2022-02-27 22:58:09 +0100

    updated dot env files

---
+++ src/components/index.js
@@ -0,0 +1,46 @@
+import React from "react";
+import Weather from './weather'
+import './index.css'
+import Hotels from './Hotels'
+import Map from './map'
+import Nav from './nav'
+import {
+  BrowserRouter as Router,
+  Switch,
+  useLocation ,
+  Route,
+  useHistory,
+  Link
+} from "react-router-dom";
+import { HotelDetails } from "./Hotels/data";
+import HotelDetailsComponent from "./Hotels/HotelDetails"
+
+
+function App() {
+    const location = useHistory()
+    return (
+        <div>
+            <Router  >
+            <div>
+<div className='weatherBk' style={{height: window.screen.height}} ></div>
+<Nav/>
+<Switch  >
+<Route path='/Map' >
+<Map/>
+</Route>
+<Route path='/hotelDetails/:id' >
+<HotelDetailsComponent/>
+</Route>
+<Route path='/Hotels' > <Hotels/> </Route>
+ <Route path='/'  >
+<Weather/>
+                    </Route>
+         
+</Switch>
+</div>
+            </Router>
+        </div>
+    )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import React from 'react'
-import reactDom from 'react-dom'
-import MyApp from './MyApp/index'
-function App() {
-    return (
-        <div>
-<MyApp/>
-        </div>
-// https://drive.google.com/file/d/1IVmhPR7q5OmbNP-I6vVcAvb6t-N7kmGz/view
-    )
-}
-reactDom.render( <App/> , document.getElementById('root') )
+import ReactDOM from 'react-dom';
+import App from './components'
+
+ReactDOM.render(
+<App/> ,
+  document.getElementById('root')
+);
